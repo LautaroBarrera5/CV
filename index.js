35d16aa1ea1af119f3efe526153aa6267d4106ca
Lautaro <lautarobarrera150@outlook.com>
2023-02-25 23:44:18 -0300

    primera subida

---
+++ index.js
@@ -0,0 +1,44 @@
+function funcionScroll1()
+ { console.log("funciona");
+
+ document.getElementById('scroll1').scrollIntoView({
+      behavior: 'smooth'
+      
+      
+ });
+}
+
+
+
+function funcionScroll2()
+ { console.log("funciona");
+
+ document.getElementById('scroll2').scrollIntoView({
+      behavior: 'smooth'
+      
+      
+ });
+}
+
+function funcionScroll3()
+ { console.log("funciona");
+
+ document.getElementById('scroll3').scrollIntoView({
+      behavior: 'smooth'
+      
+      
+ });
+}
+
+function funcionScroll4()
+ { console.log("funciona");
+
+ document.getElementById('scroll4').scrollIntoView({
+      behavior: 'smooth'
+      
+      
+ });
+}
+
+
+ 
